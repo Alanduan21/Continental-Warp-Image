450995ec496e0c330c65a60714099ee3463c94fe
Duan <uih32851@contiwan.com>
2025-06-25 14:58:26 +0800

    relocate paths of images and videos, realise image can only do one by one, fix for automation

--- warppicture.py
+++ warppicture.py
@@ -18,17 +18,24 @@ def warp_to_smile_shape(image, curvature=0.0005):
     warped = cv2.remap(image, map_x, map_y, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT)
     return warped
 
-image = cv2.imread("C:/Users/uih32851/Desktop/Image1.png")
-# image = cv2.imread("C:/Users/uih32851/Desktop/Image2.png")
+image = cv2.imread("C:/Users/uih32851/Desktop/warppicture/Image/Image1.png")
+# image = cv2.imread("C:/Users/uih32851/Desktop/warppicture/Image/Image2.png")
 
 # to debug 
 if image is None:
     print("Image not loaded. Check the file path and name.")
     exit()
 
+# to write out image
+warped = warp_to_smile_shape(image)
+cv2.imwrite("C:/Users/uih32851/Desktop/warppicture/Image/warped_smile_image_1.png", warped)
+# cv2.imwrite("C:/Users/uih32851/Desktop/warppicture/Image/warped_smile_image_2.png", warped)
+print(f"image printed out")
+
+
 # video PATH definition
-input_video_path = "C:/Users/uih32851/Desktop/video1.mp4"
-output_video_path = "C:/Users/uih32851/Desktop/output_warped_video.mp4"
+input_video_path = "C:/Users/uih32851/Desktop/warppicture/Video/video1.mp4"
+output_video_path = "C:/Users/uih32851/Desktop/warppicture/Video/output_warped_video.mp4"
 
 cap = cv2.VideoCapture(input_video_path)
 
@@ -52,11 +59,7 @@ while True:
 
     # apply warp 
     warped_frame = warp_to_smile_shape(frame)
-
-
     video_out.write(warped_frame)
-
-
     # release everything
 
 cap.release()
@@ -64,15 +67,3 @@ video_out.release()
 print(f"video printed out")
 
 
-
-
-
-
-
-
-
-
-# to write out 
-warped = warp_to_smile_shape(image)
-cv2.imwrite("C:/Users/uih32851/Desktop/warped_smile_image_1.png", warped)
-# cv2.imwrite("C:/Users/uih32851/Desktop/warped_smile_image_2.png", warped)
